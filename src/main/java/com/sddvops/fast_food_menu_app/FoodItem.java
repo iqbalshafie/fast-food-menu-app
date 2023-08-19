56f0401f1e2ca280c6cfae977db23090bb40f555
iqbalshafie <iqbal@evantek.com>
2023-08-19 06:43:26 -0700

    Initial commit

---
+++ src/main/java/com/sddvops/fast_food_menu_app/FoodItem.java
@@ -0,0 +1,23 @@
+package com.sddvops.fast_food_menu_app;
+public class FoodItem {
+    private String name;
+    private double price;
+
+    public FoodItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - $" + price;
+    }
+}
---
+++ src/main/java/com/sddvops/fast_food_menu_app/MenuApp.java
@@ -0,0 +1,10 @@
+package com.sddvops.fast_food_menu_app;
+
+public class MenuApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
