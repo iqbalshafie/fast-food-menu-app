2c1102d93561133b210dce7a2d64d162666cd76a
iqbalshafie <iqbal@evantek.com>
2023-08-24 08:51:16 -0700

    add in junit test

---
+++ src/main/java/com/sddvops/fast_food_menu_app/MenuItem.java
@@ -0,0 +1,39 @@
+package com.sddvops.fast_food_menu_app;
+
+public class MenuItem {
+	private String name;
+	private String description;
+	private double price;
+
+	public MenuItem(String name, String description, double price) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	// Getters and setters
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
---
+++ src/test/java/com/sddvops/fast_food_menu_app/MenuAppTest.java
@@ -0,0 +1,63 @@
+package com.sddvops.fast_food_menu_app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MenuAppTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testRegisterUser() {
+		MenuApp menuApp = new MenuApp();
+		menuApp.registerUser("testuser", "password123", "test@example.com");
+
+		User user = menuApp.authenticateUser("testuser", "password123");
+		assertNotNull(user);
+		assertEquals("testuser", user.getUsername());
+		assertEquals("test@example.com", user.getEmail());
+	}
+
+	@Test
+	void testAuthenticateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddMenuItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlaceOrder() {
+		User user = new User("testuser", "password123", "test@example.com");
+		List<MenuItem> items = new ArrayList<>();
+		// Populate items list with sample menu items
+		// ...
+
+		Order order = new Order(user, items);
+
+		assertEquals(user, order.getUser());
+		assertEquals(items, order.getItems());
+		assertNotNull(order.getOrderTime());
+		assertFalse(order.isPaid());
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
